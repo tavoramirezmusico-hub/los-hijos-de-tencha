61485f41d1d577dc20f218724e096c6ccfc23493
Tavo Ramírez <tavoramirezmusico@gmail.com>
2026-07-12 14:56:55 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("Sitio Oficial de Los Hijos de Tencha");
-
-const botonMenu = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-
-botonMenu.addEventListener("click", () => {
-    menu.classList.toggle("activo");
-});
