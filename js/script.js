9a43103ef43431047fcf93327c3ab70e68c31efb
Tavo Ramírez <tavoramirezmusico@gmail.com>
2026-07-12 14:49:59 -0600

    Add menu toggle functionality to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+console.log("Sitio Oficial de Los Hijos de Tencha");
 
+const botonMenu = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+
+botonMenu.addEventListener("click", () => {
+    menu.classList.toggle("activo");
+});
