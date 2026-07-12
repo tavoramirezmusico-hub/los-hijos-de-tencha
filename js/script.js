8fced702e31430aa89a0388eeed476657ce779b6
Tavo Ramírez <tavoramirezmusico@gmail.com>
2026-07-12 14:55:29 -0600

    Add event listeners to menu links for closing menu

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ const menu = document.querySelector(".menu");
 botonMenu.addEventListener("click", () => {
     menu.classList.toggle("activo");
 });
+
+const enlacesMenu = document.querySelectorAll(".menu a");
+
+enlacesMenu.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        menu.classList.remove("activo");
+    });
+});
